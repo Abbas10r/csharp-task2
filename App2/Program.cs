a590cd206eae1f194ce7516896054604c5959cf5
Abbas Azisov <abbastktl@gmail.com>
2020-01-13 22:13:18 +0600

    Добавьте файлы проекта.

---
+++ App2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int hours = Convert.ToInt32(Console.ReadLine());
+            double forhour = Convert.ToDouble(Console.ReadLine());
+            double total_salary = hours * forhour;
+            Console.WriteLine($"NUMBER = {num}");
+            Console.WriteLine($"SALARY = U$ {total_salary}");
+        }
+    }
+}
